d546e1497cf6e7bee075d9a88e8e182824c62aff
tuanvus <87792686+tuanvus@users.noreply.github.com>
2023-04-03 09:31:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use(function(req, res, next) {
 });
 
 
-
+const PORT = 3000;
 // // finally, let's start our server...
 // var server = app.listen( process.env.PORT || 3000, function(){
 //   console.log('Listening on port ' + server.address().port);
